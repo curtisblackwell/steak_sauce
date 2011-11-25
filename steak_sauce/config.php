d320ced095a1a77370b2dbdb8721e7a40cadf2b6
Curtis Blackwell <blackwell.curtis@gmail.com>
2011-11-25 12:08:40 +0900

    added license info to files

--- steak_sauce/config.php
+++ steak_sauce/config.php
@@ -1,4 +1,24 @@
 <?php
+
+/**
+ * cpb Steak Sauce
+ *
+ * An ExpressionEngine 2 plugin.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the Creative Commons Attribution-ShareAlike 3.0 Unported License
+ *
+ * @package		ExpressionEngine
+ * @subpackage	Addons
+ * @category	Plugin
+ * @author		Curtis Blackwell
+ * @copyright   Copyright (c) 2011, Curtis Blackwell (http://curtisblackwell.com)
+ * @link        http://curtisblackwell.com/expressionengine-add-ons/cpb-steak-sauce
+ * @license		http://creativecommons.org/licenses/by-sa/3.0/
+ * @since       Version 0.1
+ */
+
 if (! defined('STEAK_SAUCE_V')) {
 	define('STEAK_SAUCE_NAME', 'cpb Steak Sauce');
 	define('STEAK_SAUCE_V', '1.1');
